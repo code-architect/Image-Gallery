ee9650c660220bf70759a838bb22a2c68313d501
code-architect <codearchitectdeveloper@gmail.com>
2016-04-29 22:10:20 +0530

    created the structure of the system

---
+++ includes/footer.php
@@ -0,0 +1,24 @@
+<hr>
+
+<!-- Footer -->
+<footer>
+    <div class="row">
+        <div class="col-lg-12">
+            <p>Copyright &copy; Your Website 2014</p>
+        </div>
+    </div>
+    <!-- /.row -->
+</footer>
+
+</div>
+<!-- /.container -->
+
+<!-- jQuery -->
+<script src="admin/js/jquery.js"></script>
+
+<!-- Bootstrap Core JavaScript -->
+<script src="admin/js/bootstrap.min.js"></script>
+
+</body>
+
+</html>
