a0a98b53ec97eda032934c3dd3f43324e86d47dd
code-architect <codearchitectdeveloper@gmail.com>
2016-04-30 19:57:13 +0530

    Showing the admin menu dynamically. changed the index.php, added a filter to access the pages.

--- admin/includes/side_nav.php
+++ admin/includes/side_nav.php
@@ -1,21 +1,26 @@
 <!-- Sidebar Menu Items - These collapse to the responsive navigation menu on small screens -->
 <div class="collapse navbar-collapse navbar-ex1-collapse">
     <ul class="nav navbar-nav side-nav">
-        <li class="active">
-            <a href="index.php"><i class="fa fa-fw fa-dashboard"></i> Dashboard</a>
-        </li>
-        <li>
-            <a href="index.php?users"><i class="fa fa-fw fa-bar-chart-o"></i> Users</a>
-        </li>
-        <li>
-            <a href="index.php?upload"><i class="fa fa-fw fa-table"></i> Upload</a>
-        </li>
-        <li>
-            <a href="index.php?photos"><i class="fa fa-fw fa-edit"></i> Photos</a>
-        </li>
-        <li>
-            <a href="index.php?comments"><i class="fa fa-fw fa-edit"></i> Comments</a>
-        </li>
+<!--        <li class="active">-->
+<!--            <a href="index.php"><i class="fa fa-fw fa-dashboard"></i> Dashboard</a>-->
+<!--        </li>-->
+<!--        <li>-->
+<!--            <a href="index.php?p=users"><i class="fa fa-fw fa-bar-chart-o"></i> Users</a>-->
+<!--        </li>-->
+<!--        <li>-->
+<!--            <a href="index.php?p=upload"><i class="fa fa-fw fa-table"></i> Upload</a>-->
+<!--        </li>-->
+<!--        <li>-->
+<!--            <a href="index.php?p=photos"><i class="fa fa-fw fa-edit"></i> Photos</a>-->
+<!--        </li>-->
+<!--        <li>-->
+<!--            <a href="index.php?p=comments"><i class="fa fa-fw fa-edit"></i> Comments</a>-->
+<!--        </li>-->
+        <?php foreach($site_pages as $page){ ?>
+            <li>
+                <a href="index.php?p=<?php echo $page; ?>"><i class="fa fa-fw fa-table"></i> <?php echo ucfirst($page); ?></a>
+            </li>
+        <?php } ?>
 
         <li>
             <a href="javascript:;" data-toggle="collapse" data-target="#demo"><i class="fa fa-fw fa-arrows-v"></i> Dropdown <i class="fa fa-fw fa-caret-down"></i></a>
--- admin/index.php
+++ admin/index.php
@@ -1,32 +1,60 @@
+<?php include("init/autoload.php"); ?>
 <?php include("includes/header.php"); ?>
 <?php include("includes/top_nav.php"); ?>
 <?php include("includes/side_nav.php") ?>
 
 <?php
+//if($_SERVER['REQUEST_URI'] == "/sand_box/image_gallery/admin/index.php" ||
+//   $_SERVER['REQUEST_URI'] == "/sand_box/ecommerce_simple/public/admin/" )
+//{
+//    include("pages/main.php");                              // main page
+//}
+//elseif(isset($_GET['users']))                              // Users summery page
+//{
+//    include("pages/users.php");
+//}
+//elseif(isset($_GET['photos']))                              // Photos page
+//{
+//    include("pages/photos.php");
+//}
+//elseif(isset($_GET['upload']))                              // Upload page
+//{
+//    include("pages/upload.php");
+//}
+//elseif(isset($_GET['comments']))                            // Comments page
+//{
+//    include("pages/comments.php");
+//}
+//else
+//{
+//    include("pages/main.php");
+//}
+/*
+foreach($site_pages as $page)
+{
+    if(isset($_GET['p'])) {
+        if ($page == $_GET['p']) {
+            include(PAGES_DIR . DS . $_GET['p'] . ".php");
+            break;
+        } else {
+            include(PAGES_DIR . DS . "main.php");
+        }
+    }
+}*/
 if($_SERVER['REQUEST_URI'] == "/sand_box/image_gallery/admin/index.php" ||
-   $_SERVER['REQUEST_URI'] == "/sand_box/ecommerce_simple/public/admin/" )
+   $_SERVER['REQUEST_URI'] == "/sand_box/image_gallery/admin/" )
 {
     include("pages/main.php");                              // main page
 }
-elseif(isset($_GET['users']))                              // Users summery page
-{
-    include("pages/users.php");
-}
-elseif(isset($_GET['photos']))                              // Photos page
-{
-    include("pages/photos.php");
-}
-elseif(isset($_GET['upload']))                              // Upload page
-{
-    include("pages/upload.php");
-}
-elseif(isset($_GET['comments']))                            // Comments page
-{
-    include("pages/comments.php");
-}
-else
-{
-    include("pages/main.php");
+else if(isset($_GET['p'])){
+    if(in_array($_GET['p'], $site_pages))
+    {
+        include(PAGES_DIR.DS.$_GET['p'].".php");
+    }
+    else
+    {
+        include("pages/main.php");
+    }
 }
 ?>
 <!-- /.row -->
