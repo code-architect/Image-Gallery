fcebd0a3078c6af72562cba469002832b26b995e
code-architect <codearchitectdeveloper@gmail.com>
2016-07-18 21:51:40 +0530

    Created Comment class and few methods

--- admin/classes/Application.php
+++ admin/classes/Application.php
@@ -3,11 +3,13 @@ class Application {
 
     public $user;
     public $photo;
+    public $comment;
 
     // declare needed instances in here to use them
     public function __construct() {
         $this->user = new User();
         $this->photo = new Photo();
+        $this->comment = new Comment();
 
     }
 
---
+++ admin/classes/Comment.php
@@ -0,0 +1,86 @@
+<?php
+class Comment extends DBObject
+{
+    protected $tableName = 'comments';
+
+
+    public function create_comment($arr = array())
+    {
+        // if not an array return false
+        if(!is_array($arr))
+        {
+            return false;
+        }
+        // if empty return false
+        elseif(empty($arr))
+        {
+            return false;
+        }
+    }
+
+
+
+//-----------------------------------------------------------------------------------------------------//
+
+
+    /**
+     * check if comments for the particular image exists or not
+     * @param $image_id
+     * @return bool
+     */
+    public function comment_exists($image_id)
+    {
+        $image_id = $this->clean_data($image_id);
+        $query = "SELECT comm_author_id FROM ".$this->tableName." WHERE comm_image_id = ".$image_id;
+        $rows = $this->db->num_rows($query);
+        if($rows == true){
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+//-----------------------------------------------------------------------------------------------------//
+
+
+    /**
+     * @work find comments using photo id
+     * @param $photo_id
+     * @return array
+     */
+    public function find_comments($photo_id)
+    {
+        // cleaning given data
+        $photo_id = $this->clean_data($photo_id);
+
+        // if there comments exists for this image get comments else return false
+        if($this->comment_exists($photo_id))
+        {
+            $sql = "SELECT c.comm_id, c.comm_author_id, c.comm_body, c.comm_date, u.user_name, u.user_id ";
+            $sql .= "from " . $this->tableName . " as c INNER JOIN users as u ";
+            $sql .= "where c.comm_author_id = u.user_id ";
+            $sql .= "AND c.comm_image_id = " . $photo_id;
+
+            // execute query and return data
+            $data = $this->db->execute_query($sql);
+            return $data;
+        }else
+        {
+            return false;
+        }
+    }
+
+
+//-----------------------------------------------------------------------------------------------------//
+
+
+
+
+
+
+
+
+
+
+
+}
