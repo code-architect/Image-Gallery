ca4fdfd685dee655bee8d0b959d02bd48f01450f
code-architect <codearchitectdeveloper@gmail.com>
2016-07-19 21:26:55 +0530

    redirecting if directly accessed front_pages directory.
    created photo.php

---
+++ photo.php
@@ -0,0 +1,8 @@
+<?php include('includes/header.php'); ?>
+<?php include('includes/nav.php'); ?>
+
+<?php include_once('front_pages/photo_details.php'); ?>
+
+<?php include('includes/sidebar.php'); ?>
+<?php include('includes/footer.php'); ?>
+
